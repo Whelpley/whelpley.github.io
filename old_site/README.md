f670aa540c51479207d11ff2de79f67ad74b258c
Whelpley <whelpley@gmail.com>
2017-04-26 17:26:20 -0700

    Delete old files

--- old_site/README.md
+++
@@ -1,2 +0,0 @@
-# whelpley.github.io
-My DBC Phase 0 Github pages site
--- old_site/ruby_testpad.rb
+++
@@ -1,7 +0,0 @@
-name = "Steve"
-
-p (name + " is my original name. What is my new name?")
-
-name = gets.chomp
-
-p (name + " is my new name")
